ee3b677bc51abb88c71b0fcba08294176bc38d67
uchikakoi-eishou <kakoi.0501souta@gmail.com>
2019-11-10 09:44:05 +0900

    file split

---
+++ js/jquery.js
@@ -0,0 +1,15 @@
+// $(function(){
+
+    $('body').hide().fadeIn(1500);
+    // $(window).scroll(function(){
+    //     $('#about').each(function(){
+    //         var elemPos = $(this).offset().top,
+    //             scroll = $(window).scrollTop(),
+    //             windowHeight = $(window).height();
+    //         if (scroll > elemPos - windowHeight + 100){
+    //             $(this).addClass('scrollin');
+    //         }
+    //     });
+    // });
+
+// });
